bfe5dc4b01533fd3799e8e12ae8feb1d2321a067
Kvarcent Dekok <pavhap@gmail.com>
2020-09-25 15:49:47 +0300

    Добавляет отображение карточки объявления

--- js/main.js
+++ js/main.js
@@ -6,8 +6,21 @@ const LOCATION_Y_MAX = 630;
 const PIN_WIDTH = 50;
 const PIN_HEIGHT = 70;
 
-const mapBlock = document.querySelector(`.map`);
 const offerTypes = [`palace`, `flat`, `house`, `bungalow`];
+const offerTypesLocal = {
+  palace: {
+    ru: `Дворец`
+  },
+  flat: {
+    ru: `Квартира`
+  },
+  house: {
+    ru: `Дом`
+  },
+  bungalow: {
+    ru: `Бунгало`
+  }
+};
 const offerTimes = [`12:00`, `13:00`, `14:00`];
 const offerPossibleFeatures = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
 const offerPossiblePhotos = [
@@ -15,6 +28,7 @@ const offerPossiblePhotos = [
   `http://o0.github.io/assets/images/tokyo/hotel2.jpg`,
   `http://o0.github.io/assets/images/tokyo/hotel3.jpg`
 ];
+const mapBlock = document.querySelector(`.map`);
 const offers = createOffers();
 
 function getRandomInt(min, max) {
@@ -31,7 +45,11 @@ function getRandomArray(array, length) {
   let result = [];
 
   for (let i = 0; i < length; i++) {
-    result.push(array[getRandomInt(0, array.length - 1)]);
+    const index = getRandomInt(0, array.length - 1);
+
+    if (!(result.indexOf(array[index]) + 1)) {
+      result.push(array[index]);
+    }
   }
 
   return result;
@@ -88,6 +106,64 @@ function createPin(offer, pinTemplate) {
   return pinElement;
 }
 
+function createCard(offer, cardTemplate) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardTitle = cardElement.querySelector(`.popup__title`);
+  const cardAddress = cardElement.querySelector(`.popup__text--address`);
+  const cardPrice = cardElement.querySelector(`.popup__text--price`);
+  const cardType = cardElement.querySelector(`.popup__type`);
+  const cardCapacity = cardElement.querySelector(`.popup__text--capacity`);
+  const cardTime = cardElement.querySelector(`.popup__text--time`);
+  const cardFeatures = cardElement.querySelector(`.popup__features`);
+  const cardDescription = cardElement.querySelector(`.popup__description`);
+  const cardPhotos = cardElement.querySelector(`.popup__photos`);
+  const cardPhoto = cardPhotos.querySelector(`.popup__photo`);
+  const cardAvatar = cardElement.querySelector(`.popup__avatar`);
+  const {title, address, price, type, rooms, guests, checkin, checkout, features, description, photos} = offer.offer;
+
+  cardTitle.textContent = title;
+  cardAddress.textContent = address;
+  cardPrice.textContent = `${price}₽/ночь`;
+  cardType.textContent = offerTypesLocal[type].ru;
+  cardCapacity.textContent = `${rooms} комнаты для ${guests} гостей`;
+  cardTime.textContent = `Заезд после ${checkin}, выезд до ${checkout}`;
+
+  for (let i = 0; i < features.length; i++) {
+    const feature = cardFeatures.querySelector(`.popup__feature--${features[i]}`);
+
+    feature.textContent = features[i];
+  }
+
+  for (let i = 0; i < cardFeatures.children.length; i++) {
+    if (cardFeatures.children[i].textContent === ``) {
+      cardFeatures.children[i].classList.add(`hidden`);
+    }
+  }
+
+  cardDescription.textContent = description;
+
+  if (photos.length === 0) {
+    cardPhoto.classList.add(`hidden`);
+  } else if (photos.length === 1) {
+    cardPhoto.src = photos[0];
+  } else if (photos.length > 1) {
+    cardPhoto.src = photos[0];
+    addPhotos(cardPhoto, cardPhotos, photos);
+  }
+
+  cardAvatar.src = offer.author.avatar;
+
+  return cardElement;
+}
+
+function createPhoto(src, photoTemplate) {
+  const photoElement = photoTemplate.cloneNode(true);
+
+  photoElement.src = src;
+
+  return photoElement;
+}
+
 function addPins() {
   const pinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
   const pinsFragment = document.createDocumentFragment();
@@ -100,6 +176,24 @@ function addPins() {
   mapPinsBlock.appendChild(pinsFragment);
 }
 
+function addPhotos(photoTemplate, cardPhotos, photos) {
+  const photosFragment = document.createDocumentFragment();
+
+  for (let i = 1; i < photos.length; i++) {
+    photosFragment.appendChild(createPhoto(photos[i], photoTemplate));
+  }
+
+  cardPhotos.appendChild(photosFragment);
+}
+
+function addCard() {
+  const cardTemplate = document.querySelector(`#card`).content.querySelector(`.map__card`);
+  const filtersContainer = mapBlock.querySelector(`.map__filters-container`);
+
+  mapBlock.insertBefore(createCard(offers[0], cardTemplate), filtersContainer);
+}
+
 mapBlock.classList.remove(`map--faded`);
 
 addPins();
+addCard();
